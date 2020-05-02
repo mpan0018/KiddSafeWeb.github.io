b7372e19b5c81b70d7e25c0bfb652f658034caf2
Manan <mananpandya2801@gmail.com>
2020-05-02 14:57:23 +1000

    css clip-path to add

---
+++ js/3deffects.js
@@ -0,0 +1,10 @@
+function blur_background(){
+    console.log("blur background called");
+    document.getElementById("background_image").style.opacity = "0.2";
+}
+
+function restore_background(){
+    console.log("blur background called");
+    document.getElementById("background_image").style.opacity = "1";
+}
+
